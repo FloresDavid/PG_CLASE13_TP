8b5580b33f01e75d546358f20c08b955f612b987
FloresDavid <Davidabelflores@gmail.com>
2023-09-15 11:09:31 -0300

    añado razon de por que tomate se fruta

--- tomate.js
+++ tomate.js
@@ -1,1 +1,2 @@
 console.log("tomate");
+console.log("la clasificación botánica, que se basa en las características filosóficas de la planta como su estructura o función, el tomate es una fruta porque contiene pequeñas semillas en el medio y crece de la flor de la planta de tomate.");
